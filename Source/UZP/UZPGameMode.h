9521cba0b365b104792ed3905496a92920dcfeef
Abdallah Shakhatreh <abdshak@hotmail.com>
2024-06-08 17:59:25 +0300

    Project Creation
    Project Creation

---
+++ Source/UZP/UZPGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UZPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUZPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUZPGameMode();
+};
+
+
+
